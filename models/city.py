b570f5488d2248630f7369e649f51770415452cb
“Akanni23“ <muhasy2k@yahoo.com>
2024-07-29 05:23:05 -0700

    update

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, String, 
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ ='cities'
+    state_id = Column(String(128), nullable=False)
+    name = Column(String(60), ForeignKey('state.id'), nullable=False)
+    place = relationship('place', backref='cities', cascade='all, delete-orphan')
